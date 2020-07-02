630ffdda8997bda1bdcc5f32adb85b31d48b7232
putuduta <putu.duta@gmail.com>
2020-07-02 11:37:35 +0700

    Add frontend

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav class = "navbar navbar-light bg-light" >
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/create" className="nav-link ml-auto">Create Exercise Log</Link>
+        <Link to="/user" className="nav-link">Create User</Link>
+      </nav>
+    );
+  }
+}
